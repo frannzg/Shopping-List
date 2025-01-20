df5a04116f33b3d83309890ed6873aa9d8c09788
frannzg <148803161+frannzg@users.noreply.github.com>
2025-01-20 17:11:44 +0100

    20/01/2025 V1

--- app/src/main/java/com/frannzg/shopping_list/Product.java
+++ app/src/main/java/com/frannzg/shopping_list/Product.java
@@ -1,43 +1,29 @@
 package com.frannzg.shopping_list;
 
 public class Product {
+    private String name;
+    private boolean isBought;
 
-    private String name;      // Nombre del producto
-    private boolean isChecked; // Estado del producto (marcado o no)
-
-    // Constructor vacío
-    public Product() {
-    }
-
-    // Constructor con parámetros para inicializar el producto con su nombre y estado
-    public Product(String name, boolean isChecked) {
+    // Constructor
+    public Product(String name, boolean isBought) {
         this.name = name;
-        this.isChecked = isChecked;
+        this.isBought = isBought;
     }
 
-    // Getter para obtener el nombre del producto
+    // Getters y setters
     public String getName() {
         return name;
     }
 
-    // Setter para establecer el nombre del producto
     public void setName(String name) {
         this.name = name;
     }
 
-    // Getter para verificar si el producto está marcado (checked)
-    public boolean isChecked() {
-        return isChecked;
+    public boolean isBought() {
+        return isBought;
     }
 
-    // Setter para actualizar el estado del producto (marcado o no marcado)
-    public void setChecked(boolean checked) {
-        isChecked = checked;
-    }
-
-    // Método toString() para representar el producto como una cadena
-    @Override
-    public String toString() {
-        return name + (isChecked ? " (Marcado)" : " (No marcado)");
+    public void setBought(boolean bought) {
+        isBought = bought;
     }
 }
