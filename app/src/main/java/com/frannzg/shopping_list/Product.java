34900d7931b8ae535a6fae3c080cf88ff8a5becc
frannzg <148803161+frannzg@users.noreply.github.com>
2025-02-06 23:27:39 +0100

    06/02/2025 V1

--- app/src/main/java/com/frannzg/shopping_list/Product.java
+++ app/src/main/java/com/frannzg/shopping_list/Product.java
@@ -1,16 +1,28 @@
 package com.frannzg.shopping_list;
 
 public class Product {
+    private String id; // Identificador único del producto en Firebase
     private String name;
     private boolean isBought;
 
+    public Product() {}
+
     // Constructor
-    public Product(String name, boolean isBought) {
+    public Product(String id, String name, boolean isBought) {
+        this.id = id;
         this.name = name;
         this.isBought = isBought;
     }
 
     // Getters y setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
     public String getName() {
         return name;
     }
@@ -23,7 +35,7 @@ public class Product {
         return isBought;
     }
 
-    public void setBought(boolean bought) {
-        isBought = bought;
+    public void setBought(boolean isBought) {
+        this.isBought = isBought;
     }
 }
